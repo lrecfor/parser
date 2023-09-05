10566abd2e5f15cefca63ad1362f2e0a9ff9fd0d
lrecfor <lalarecfor@gmail.com>
2023-09-05 20:56:45 +0500

    finished parser

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ class Parser:
 
     def __init__(self):
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 "
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/116.0.0.0"
                           "Safari/537.36"
         }
         self.time = None
@@ -18,20 +19,12 @@ class Parser:
         response = session.get(f"https://hornews.com/news/", headers=self.headers)
 
         bs = BeautifulSoup(response.text, "html.parser")
-        self.time = bs.find_all('span', 'news__category')
-        # for _ in self.time:
-        #     print(_.text)
 
-        self.title = bs.find_all('p', 'news__title')
-        # for _ in self.title:
-        #     print(_.text)
-
-        news_links = []
-        for a_tag in bs.find_all('a'):
-            href = a_tag.attrs.get("href")
-            if href == "" or href is None or href[:6] != "/news/" or len(href) == 6:
-                continue
-            news_links.append(href)
+        news_block = bs.find('div', 'row')
+        news_links = [a_tag.attrs.get("href") for a_tag in news_block.find_all('a') if a_tag.attrs.get("href")
+                      and a_tag.attrs.get("href") != ""]
+        dict_keys = {}.fromkeys(news_links)
+        news_links = list(dict_keys.keys())
 
         for link in news_links:
             session_ = requests.Session()
@@ -39,15 +32,18 @@ class Parser:
 
             bs = BeautifulSoup(response_.text, "html.parser")
 
+            self.time = bs.find('time', 'heading-block__time').text
+            print(self.time)
+
+            self.title = bs.find('h1', 'heading-block__title').text
+            print(self.title)
+
             ds = bs.find('div', 'news-item__text user-text')
-            result = []
-            for p_ in ds.find_all('p'):
-                result.append(p_.text)
+            self.new_text = [_.text for _ in ds.find_all('p')]
             ds = bs.find('div', 'news')
-            result_ = []
-            for p_ in ds.find_all('p'):
-                result_.append(p_.text)
-            print("".join(result[:-len(result_)]))
+            self.new_text = "".join(self.new_text[:-len([_.text for _ in ds.find_all('p')])])
+
+            print(self.new_text)
 
     def get_news(self):
         self.parse_news()
